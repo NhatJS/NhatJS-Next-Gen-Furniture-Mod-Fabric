07b44039935d1cbae23c0258c5515d8176aafa2e
NhatJS <nhatjsouthfall7@gmail.com>
2025-09-08 00:23:44 +0700

    improved chair seat

--- src/main/java/net/nhatjs/nextgen_furniture/entity/client/ChairBlockEntity.java
+++ src/main/java/net/nhatjs/nextgen_furniture/entity/client/ChairBlockEntity.java
@@ -1,10 +1,12 @@
 package net.nhatjs.nextgen_furniture.entity.client;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.data.DataTracker;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.world.World;
+import net.nhatjs.nextgen_furniture.block.ChairBlock;
 
 public class ChairBlockEntity extends Entity {
 
@@ -31,5 +33,27 @@ public class ChairBlockEntity extends Entity {
     protected void removePassenger(Entity passenger) {
         super.removePassenger(passenger);
         this.kill();
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+
+        if (!this.getWorld().isClient) {
+            BlockState below = this.getWorld().getBlockState(this.getBlockPos());
+            if (!(below.getBlock() instanceof ChairBlock)) {
+                if (this.hasPassengers()) this.getPassengerList().forEach(p -> p.stopRiding());
+                this.discard();
+            }
+        }
+    }
+
+    @Override
+    public void remove(RemovalReason reason) {
+        if (!this.getWorld().isClient) {
+            if (this.hasPassengers()) this.getPassengerList().forEach(p -> p.stopRiding());
+            this.removeAllPassengers();
+        }
+        super.remove(reason);
     }
 }
